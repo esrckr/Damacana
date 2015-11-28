8ab04f7ee253c5f7b12a773ebe4493fa953a04db
Esra ÇAKIR <esrckr_@hotmail.com>
2015-11-28 20:54:20 +0200

    nesne yaratıldı
    nesneler tanımlandı

--- Damacana/Controllers/HomeController.cs
+++ Damacana/Controllers/HomeController.cs
@@ -1,16 +1,41 @@
-﻿using System;
+﻿using Damacana.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace Damacana.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            // crete an instance product
+            Product product1 = new Product();
+            
+                product1.Id = 1;
+                product1.Name = "Erikli 19L";
+                product1.Price = (decimal)10.5;
+            
+       
+           
+
+           Product product2 = new Product();
+
+            product2.Id = 2;
+            product2.Name = "Erikli 5L";
+            product2.Price = (decimal)5.5;
+
+
+            //send product to the view engine
+            List<Product> products = new List<Product>();
+            products.Add(product1);
+            products.Add(product2);
+
+            return View( products);
         }
 
         public ActionResult About()
---
+++ Damacana/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Damacana.Models
+{
+    //definition
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
